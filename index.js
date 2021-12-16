0bcf7ff742ee9d43d40dc43eebba6dbbf9494fb8
JLee <thishereisjenn@gmail.com>
2021-12-15 20:49:21 -0800

    added prompts

--- index.js
+++ index.js
@@ -8,29 +8,76 @@ const manQuestions = [
 	{
 		type: 'input',
 		message: 'Please enter the team managers name:  ',
-		name: 'managerName',
+		name: 'employeeName',///
 	},
 	{
 		type: 'input',
 		message: 'Please enter the team managers employee ID: ',
-		name: 'managerID',
+		name: 'employeeID',///
 	},
 	{
 		type: 'input',
 		message: 'Please enter the team managers email address: ',
-		name: 'managerEmail',
+		name: 'employeeEmail',///
 	},
 	{	
 		type: 'input',
 		message: 'Please enter the team managers office number: ',
-		name: 'managerOffice',
+		name: 'employeeOffice',///
 	},
-	{
-		type: 'list',
-		message: 'Would you like to add another team member? ',
-		name: 'additional',
-		choices: ['Engineer', 'Intern', 'Finish' ],
-	},
-	
-
 ]
+		///
+const employeeSelect = [
+    {
+        type: 'list',
+        name: 'employeeType',///
+        message: 'Please Select the type of employee you would like to add to your team or Done to finish:',
+        choices: ["Engineer", "Intern", new inquirer.Separator(), "Done"]
+    }
+]
+
+const engineerQuestions = [
+    {
+        type: 'input',
+        name: 'employeeName',
+        message: 'Please enter your engineers name: '
+    },
+    {
+        type: 'input',
+        name: 'employeeID',
+        message: 'Please enter your engineers ID: '
+    },
+    {
+        type: 'input',
+        name: 'employeeEmail',
+        message: 'Please enter your engineers email: '
+    },
+    {
+        type: 'input',
+        name: 'employeeGit',
+        message: 'Please enter your engineers GitHub username: '
+    }
+];
+
+const intQry = [
+    {
+        type: 'input',
+        name: 'employeeName',
+        message: 'Please enter the name of your intern:'
+    },
+    {
+        type: 'input',
+        name: 'employeeID',
+        message: 'Please enter the employee ID of your intern:'
+    },
+    {
+        type: 'input',
+        name: 'employeeEmail',
+        message: 'Please enter the email of your intern:'
+    },
+    {
+        type: 'input',
+        name: 'employeeSchool',
+        message: 'Please enter the school name of your intern:'
+    }
+];
