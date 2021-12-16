60f23655408006703af9d42f9d557d8292c36369
JLee <thishereisjenn@gmail.com>
2021-12-15 20:39:30 -0800

    added lib js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
 
-const questions = [
+const manQuestions = [
 	{
 		type: 'input',
 		message: 'Please enter the team managers name:  ',
