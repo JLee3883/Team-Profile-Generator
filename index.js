96fa317222b1d4b9045a82fa6682e59a2996405a
JLee <thishereisjenn@gmail.com>
2021-12-15 20:21:08 -0800

    added employee.js content

--- index.js
+++ index.js
@@ -7,39 +7,30 @@ const Manager = require("./lib/Manager");
 const questions = [
 	{
 		type: 'input',
-		message: 'What is the team managers name? ',
-		name: 'manager',
+		message: 'Please enter the team managers name:  ',
+		name: 'managerName',
 	},
 	{
 		type: 'input',
-		message: 'Enter the instructions for additional questions: ',
+		message: 'Please enter the team managers employee ID: ',
+		name: 'managerID',
+	},
+	{
+		type: 'input',
+		message: 'Please enter the team managers email address: ',
+		name: 'managerEmail',
+	},
+	{	
+		type: 'input',
+		message: 'Please enter the team managers office number: ',
+		name: 'managerOffice',
+	},
+	{
+		type: 'list',
+		message: 'Would you like to add another team member? ',
 		name: 'additional',
-},
-{
-	type: 'input',
-	message: 'Enter the instructions for additional questions: ',
-	name: 'additional',
-},
-{
-	type: 'input',
-	message: 'Enter the instructions for additional questions: ',
-	name: 'additional',
-},
-{
-	type: 'input',
-	message: 'Enter the instructions for additional questions: ',
-	name: 'additional',
-},
-	{
-			type: 'list',
-			message: 'Choose your License:',
-			name: 'license',
-			choices: ['Apache_2.0', 'BSD_2--Clause', 'CC0_1.0', 'MIT', 'MPL_2.0' ],
+		choices: ['Engineer', 'Intern', 'Finish' ],
 	},
-	{
-			type: 'input',
-			message: 'Enter the instructions for additional questions: ',
-			name: 'additional',
-	},
+	
 
 ]
